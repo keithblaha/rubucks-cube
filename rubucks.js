8804e996c00f416f26dd88253c6eca47de0b8964
Keith <keith@keith-Dell-System-XPS-15Z.(none)>
2012-03-24 23:28:10 -0700

    shaders

--- rubucks.js
+++ rubucks.js
@@ -45,7 +45,30 @@ function rubucks() {
 	
 	this.InitShaders = function() {
 
-		
+		var v = document.getElementById("vertex").firstChild.nodeValue;
+		var f = document.getElementById("fragment").firstChild.nodeValue;
+		 
+		var vs = gl.createShader(gl.VERTEX_SHADER);
+		gl.shaderSource(vs, v);
+		gl.compileShader(vs);
+		 
+		var fs = gl.createShader(gl.FRAGMENT_SHADER);
+		gl.shaderSource(fs, f);
+		gl.compileShader(fs);
+		 
+		program = gl.createProgram();
+		gl.attachShader(program, vs);
+		gl.attachShader(program, fs);
+		gl.linkProgram(program);
+
+		gl.useProgram(program);
+
+		program.uColor = gl.getUniformLocation(program, "uColor");
+		gl.uniform4fv(program.uColor, [0.0, 0.3, 0.0, 1.0]);
+
+		program.aVertexPosition = gl.getAttribLocation(program, "aVertexPosition");
+		gl.enableVertexAttribArray(program.aVertexPosition);
+		gl.vertexAttribPointer(program.aVertexPosition, itemSize, gl.FLOAT, false, 0, 0);
 	}
 
 	this.Run = function() {
